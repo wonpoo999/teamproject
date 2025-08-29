92a1ab33cb779a373e757e6133963f1413c2e222
yuna <yuna5470@naver.com>
2025-08-29 16:44:12 +0900

    회원가입, 로그인, 카메라, 기본 틀

---
+++ frontend/App.js
@@ -0,0 +1,19 @@
+import { NavigationContainer } from '@react-navigation/native'
+import { SafeAreaProvider } from 'react-native-safe-area-context'
+import RootNavigator from './src/navigation/RootNavigator'
+import { AuthProvider } from './src/context/AuthContext'
+import { StatusBar } from 'expo-status-bar'
+
+export default function App(){
+  return (
+   <SafeAreaProvider>
+  <StatusBar style="light" backgroundColor="#111827" />
+  <AuthProvider>
+    <NavigationContainer>
+      <RootNavigator/>
+    </NavigationContainer>
+  </AuthProvider>
+</SafeAreaProvider>
+
+  )
+}
