2247ef9cd44a8662bb19820b8b483932922e7f3b
kimjunwoo <popple1101@naver.com>
2025-09-01 11:08:27 +0900

    customers.sql : 수정 및 주석 추가, CorsConfig.java : Native PORT 8081 추가

--- backend/src/main/resources/db/customers.sql
+++ backend/src/main/resources/db/customers.sql
@@ -1,6 +1,9 @@
--- gender
-ALTER TABLE customers DROP CONSTRAINT SYS_C007401;
+-- gender 제약 조건 수정 및 추가
 
+-- ✅ 각자 gender 제약 조건 gender CONSTRAINT_NAME SYS_XXXX 값 넣어서 DROP하기
+ALTER TABLE customers DROP CONSTRAINT SYS_;
+
+-- gender 컬럼의 값 : M or F 만 허용
 ALTER TABLE customers
   ADD CONSTRAINT ck_customers_gender
   CHECK (gender IN ('M', 'F'));
