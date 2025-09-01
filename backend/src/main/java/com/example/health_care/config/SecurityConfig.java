79b31d66343d9d57be116a6f2529d0e958907b5b
GAMGO <gamgo445@gmail.com>
2025-09-01 17:19:23 +0900

    login-function

--- backend/src/main/java/com/example/health_care/config/SecurityConfig.java
+++ backend/src/main/java/com/example/health_care/config/SecurityConfig.java
@@ -4,17 +4,22 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
+import com.example.health_care.service.CustomersService;
+
 @RequiredArgsConstructor
 @Configuration
 public class SecurityConfig {
-
+private CustomersService customersService;
     // swagger 문서 접근 허용 목록
     private static final String[] SWAGGER_WHITELIST = {
             "/v3/api-docs/**",
@@ -28,6 +33,7 @@ public class SecurityConfig {
             "/api/auth/**",     // 로그인/회원가입 등
             "/error"            // 스프링 기본 에러 엔드포인트
     };
+
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
@@ -63,4 +69,9 @@ public class SecurityConfig {
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
     }
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration) throws Exception {
+        return authenticationConfiguration.getAuthenticationManager();
+    }
+    
 }
