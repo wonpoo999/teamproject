050e4efa2308fb888b878a5c2979a6072ba243bc
kimjunwoo <popple1101@naver.com>
2025-09-02 22:06:00 +0900

    팀장님 body/foodinfo/goal.sql db폴더에 추가

---
+++ backend/src/main/resources/db/foodInfo.sql
@@ -0,0 +1,5 @@
+CREATE TABLE foodInfo (
+    food_name VARCHAR2(100),
+    calories NUMBER(5),
+    count NUMBER(5)
+);
---
+++ backend/src/main/resources/db/goal.sql
@@ -0,0 +1,6 @@
+CREATE TABLE goal (
+    id VARCHAR2(100) PRIMARY KEY NOT NULL,
+    target_weight NUMBER(4, 1) NOT NULL,
+    target_calories NUMBER(5) NOT NULL,
+    FOREIGN KEY (id) REFERENCES customers(id) ON DELETE CASCADE
+);
