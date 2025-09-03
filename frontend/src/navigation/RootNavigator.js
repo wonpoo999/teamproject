f18b3c63d4bd265c3dd9b92847cfaa7ac6f030d1
yuna <yuna5470@naver.com>
2025-09-03 11:09:34 +0900

    goal screen

--- frontend/src/navigation/RootNavigator.js
+++ frontend/src/navigation/RootNavigator.js
@@ -2,16 +2,18 @@ import { createNativeStackNavigator } from '@react-navigation/native-stack'
 import HomeScreen from '../screens/HomeScreen'
 import LoginScreen from '../screens/LoginScreen'
 import SignupScreen from '../screens/SignupScreen'
-import CameraScreen from '../screens/CameraScreen'
 import SettingsScreen from '../screens/SettingsScreen'
+import CameraScreen from '../screens/CameraScreen'
+import GoalScreen from '../screens/GoalScreen'
 
 const Stack = createNativeStackNavigator()
 
 export default function RootNavigator() {
   return (
-    <Stack.Navigator initialRouteName="Signup" screenOptions={{ headerShown: false }}>
-      <Stack.Screen name="Signup" component={SignupScreen} />
+    <Stack.Navigator screenOptions={{ headerShown: false }} initialRouteName="Login">
       <Stack.Screen name="Login" component={LoginScreen} />
+      <Stack.Screen name="Signup" component={SignupScreen} />
+      <Stack.Screen name="Goal" component={GoalScreen} />
       <Stack.Screen name="Home" component={HomeScreen} />
       <Stack.Screen name="Camera" component={CameraScreen} />
       <Stack.Screen name="Settings" component={SettingsScreen} />
